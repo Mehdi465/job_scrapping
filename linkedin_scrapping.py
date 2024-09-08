ecd50980a2a903116c659d663014412c084a83ba
Mehdi465 <mehdi.fellahi06@gmail.com>
2024-09-08 16:00:36 +0200

    ended Linkedin part

--- linkedin_scrapping.py
+++ linkedin_scrapping.py
@@ -65,7 +65,7 @@ class JobOffer:
             {self.link}
         </code>
         <code>
-            {self.website}
+            <a href="{self.website}">click here to see the offer</a> 
         </code>
         </p>
         """
@@ -74,16 +74,20 @@ class JobOffer:
         popup1 = folium.Popup(iframe, max_width=500)
 
         #create the Marker
-        marker = folium.Marker(
-            # add random to make markers not ath the same place, bc i cant extract the reak address
-            location=[location_xy.latitude + draw_gaussian_in_range(), location_xy.longitude + draw_gaussian_in_range()],
-            tooltip=f"{self.job_name}",
-            popup=popup1,
-            icon=folium.Icon(icon="cloud"),
-        )
+        if location_xy is not None:
+            marker = folium.Marker(
+                # add random to make markers not ath the same place, bc i cant extract the reak address
+                location=[location_xy.latitude + draw_gaussian_in_range(), location_xy.longitude + draw_gaussian_in_range()],
+                tooltip=f"{self.job_name}",
+                popup=popup1,
+                icon=folium.Icon(icon="cloud"),
+            )
 
-        return marker
+            return marker
+        else:
+            return None
 
+            
 # stores reponses to analyze them
 class Response:
     def __init__(self,website : JobWebSite, content) -> None:
@@ -93,21 +97,19 @@ class Response:
     def request_succeeded(self):
         return self.content.status_code == 200
 
-    def get_info_one_job(html_part_job,soup) -> JobOffer:
+    def get_info_one_job(card) -> JobOffer:
         """for one job in the page, extract all related info"""
-
-        # Parse the HTML
-        #soup = BeautifulSoup(html_part_job, 'html.parser')
+        
         # Extract job ID from the 'data-entity-urn' attribute
-        job_id = soup.find('div', class_='job-search-card')['data-entity-urn'].split(':')[-1]
+        job_id = card['data-entity-urn'].split(':')[-1]
         # Extract job link from the 'href' attribute
-        job_link = soup.find('a', class_='base-card__full-link')['href']
+        job_link = card.find('a', class_='base-card__full-link')['href']
         # Extract job name
-        job_name = soup.find('h3', class_='base-search-card__title').text.strip()
+        job_name = card.find('h3', class_='base-search-card__title').text.strip()
         # Extract company name
-        company_name = soup.find('h4', class_='base-search-card__subtitle').text.strip()
+        company_name = card.find('h4', class_='base-search-card__subtitle').text.strip()
         # Extract job location
-        location = soup.find('span', class_='job-search-card__location').text.strip()
+        location = card.find('span', class_='job-search-card__location').text.strip()
 
         job_result = JobOffer(job_name,company_name,job_link,location,salary=None,id=job_id,website=None)
 
@@ -123,7 +125,7 @@ class Response:
         job_cards = soup.find_all('div', class_="base-card relative w-full hover:no-underline focus:no-underline base-card--link base-search-card base-search-card--link job-search-card")
 
         for job_card in job_cards:
-            jobs.append(Response.get_info_one_job(job_card,soup))
+            jobs.append(Response.get_info_one_job(job_card))
 
         return jobs
 
@@ -198,7 +200,7 @@ if __name__ == "__main__":
     # INPUTS
     job_title = "software engineer"
     job_title = job_title.replace(" ","%20") # in url request " " is replaced by "%20"
-    location = "Toronto"
+    location = "Canada"
     sites = [JobWebSite.Linkedin]
 
 
@@ -223,8 +225,11 @@ if __name__ == "__main__":
     for job in all_jobs:
         # create marker and add it to the map
         marker = job.create_marker()
-        marker.add_to(m)
-        print(f"job {job.id} added")
+        if marker is not None:
+            marker.add_to(m)
+            print(f"job {job.id} added")
+        else:
+            print("Coulnd not place the job offer on the map")
         
     m.save("results/index.html")    
  
